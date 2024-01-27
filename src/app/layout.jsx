62bebf44949af04a840d45e3ddabf17dfe6287f9
lentils <vancekuikahi@gmail.com>
2024-01-26 20:21:03 -0800

    added favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,14 +1,25 @@
+import NavBar from '@/components/NavBar'
 import { orbitron } from './fonts'
 import { work_sans } from './fonts'
 import './globals.css'
 
+export const metadata = {
+    icons: {
+        icon: './favicon.ico'
+    }
+}
+
 export default function RootLayout( {children} ) {
     return (
         <html lang='en' className={`${work_sans.variable} ${orbitron.variable}`}>
-            <main>
-                {children}
-            </main>
-
+            <body className="bg-orange-50 flex flex-col px-4 py-2 min-h-screen">
+                <header>
+                    <NavBar />
+                </header>
+                <main>
+                    {children}
+                </main>
+            </body>
         </html>
     )
 }
